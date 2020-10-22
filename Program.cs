d9bf43a79aef7049ffdbd569c4552106779b7354
SomovMaxim <71885497+SomovMaxim@users.noreply.github.com>
2020-10-23 00:11:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3workSomov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x, y, z, Result;
+            Console.WriteLine("Введите x:");
+            x = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y:");
+            y = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Введите z:");
+            z = bool.Parse(Console.ReadLine());
+            Result = !(!x && y) || (x && !z);
+            Console.WriteLine(Result);
+            Console.ReadKey();
+        }
+    }
+}
